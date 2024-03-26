8a27694d77362aafd9b3b3e1eeee904ea8727daa
denis moravec <denis.moravec@educanet.cz>
2024-03-26 09:09:52 +0100

    ...

--- countries/src/app/page.js
+++ countries/src/app/page.js
@@ -1,11 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 import Link from "next/link";
+import styles from "./globals.css";
 
 export default function Home() {
     return (
         <>
+            <p>Click here:</p>
             <Link href="/countries">
                 <p>Countries</p>
             </Link>
